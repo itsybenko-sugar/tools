af17ad1376764eb25e0907b176cbb2996332e13a
Igor Tsybenko <itsybenko@sugarcrm.com>
2016-08-03 16:18:31 +0300

    upd

--- src/App/App.php
+++ src/App/App.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
-
+/**
+ * Sample class App
+ * @package App
+ */
 class App
 {
     private $mult = 2;
@@ -16,8 +20,17 @@ class App
         if (!empty($mult) && is_numeric($mult)) {
             $this->mult = (int) $mult;
         }
+
+        // use library
+        $dump = new UsernamePasswordToken('qwe', 'qwe', 'qwe');
     }
 
+    /**
+     * Sample methof
+     *
+     * @param int $number
+     * @return int
+     */
     public function mult($number)
     {
         return (int)$number * $this->mult;
